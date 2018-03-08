c50d63520f4c85b94751f2ed6b4a9d56c5a78b65
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2018-03-07 22:32:10 -0300

    Add files via upload

---
+++ wordshell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Wordlist Creator
+string=( {a..z} )
+for x in ${string[@]}; do
+for y in ${string[@]}; do
+for z in ${string[@]}; do
+for w in ${string[@]}; do
+for k in ${string[@]}; do
+#for q in ${string[@]}; do
+echo $x$y$z$w$k
+
+done done done done done
+
+
